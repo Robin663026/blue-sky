201a57a875dde08676aed5916215c1fcb02856c9
Robin663026 <1067722816@qq.com>
2018-01-16 19:21:40 +0800

    第一个页面

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="header">
+      i am header
+    </div>
+    <div class="tab">
+      i am nav
+    </div>
+    <div class="content">
+      i am content
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
+export default{
+  data(){
+    return {
+
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/init.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+<span id="im">
+  <img src="../assets/img/54_img.png" alt="">
+</span>
+    <span id="md">蓝穹</span>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'init',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello{
+    position:relative;
+  }
+
+  #im{
+    position:absolute;
+    left:0;
+    right:0;
+    margin-left:auto;
+    margin-right:auto;
+    width:360px;
+    height:248px;
+    top:0;
+    margin-top:20px;
+  }
+  #md{
+    top:0;
+    position:absolute;
+    text-align:center;
+    margin-top:270px;
+    left:0;
+    right:0;
+    margin-left:auto;
+    margin-right:auto;
+    color:#008CFF;
+    font-size:56px;
+    line-height:78px;
+    letter-spacing:24px;
+    font-family:PingFangSC-Regular;
+  }
+</style>
