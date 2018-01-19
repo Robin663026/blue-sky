bf8d8de9c56251be6ff0cd0f152866d6d721afa2
Robin663026 <1067722816@qq.com>
2018-01-19 14:21:46 +0800

    fix header and footer

--- src/components/foots.vue
+++ src/components/foots.vue
@@ -2,19 +2,19 @@
   <div class="main container">
   <div class="tab1">
     <div class="tab-item1 ">
-      <router-link class="change1" to="/news"></router-link>
+      <router-link class="change1" to="/news"><img src="../assets/img/8_icon_news_blue.png" alt=""></router-link>
     </div>
     <div class="tab-item2">
-      <router-link class='change2' to="/order"></router-link>
+      <router-link class='change2' to="/order"><img src="../assets/img/9_icon_order_grey.png" alt=""></router-link>
     </div>
     <div class="tab-item3">
-      <router-link class='change3' to="/me"></router-link>
+      <router-link class='change3' to="/me"><img src="../assets/img/10_icon_me_grey.png" alt=""></router-link>
     </div>
   </div>
   <div class="tab">
-    <div class="tab-item1"><router-link  to="/news">新闻</router-link></div>
-    <div class="tab-item2"><router-link to="/order">订阅</router-link></div>
-    <div class="tab-item3"><router-link to="/me">我</router-link></div>
+    <div class="tab-item4"><router-link  to="/news">新闻</router-link></div>
+    <div class="tab-item5"><router-link to="/order">订阅</router-link></div>
+    <div class="tab-item6"><router-link to="/me">我</router-link></div>
   </div>
   </div>
 </template>
@@ -28,6 +28,10 @@
 </script>
 
 <style  lang="less">
+  @font-face{
+    font-family:PingFangSC-Semibold;
+    src:url('../assets/fonts/pfs.ttf');
+  }
   @baseFontSize: 75;
   .px2rem(@name, @px){
       @{name}: @px / @baseFontSize * 1rem;
@@ -35,47 +39,46 @@
   .main {
     border-top: 1px solid #E2E4E6;
     .px2rem(height,100);
-
-
+    .px2rem(width,750);
     .tab1 {
-      .px2rem(margin-top,13);
+      .px2rem(margin-top,14);
       .px2rem(margin-bottom,7);
       .px2rem(margin-left,146);
       .px2rem(margin-right,150);
       .px2rem(height,48);
-      background: #FFF;
+
       .tab-item1 {
         float:left;
-        background-image:url("../assets/img/8_icon_news_blue.png");
         .px2rem(width,48);
         .px2rem(margin-right,157);
         }
       .tab-item2{
         float:left;
-        background-image:url("../assets/img/9_icon_order_grey.png");
         .px2rem(width,48);
         .px2rem(margin-right,153);
       }
       .tab-item3{
         float:left;
-        background-image:url("../assets/img/10_icon_me_grey.png");
         .px2rem(width,48);
+
+
 
       }
     }
-    /*background-img:url("")*/
+
     .tab {
 
       .px2rem(margin-bottom,10);
-      display: flex;
-      width: 100%;
+      .px2rem(margin-left,148);
+      .px2rem(margin-right,162);
+      .px2rem(height,22);
 
-      .tab-item {
-        flex: 1;
-        text-align: center;
+      .tab-item4 {
+        float:left;
+        .px2rem(width,45);
+        .px2rem(margin-right,160);
         font-family: PingFangSC-Regular;
-        font-size:11px;
-
+        font-size:22px;
         color: #A5A5A5;
         & > a {
 
@@ -83,10 +86,50 @@
           color: #A5A5A5;
         }
       }
+      [data-dpr="2"].tab-item4{
+        font-size:44px;
+      }
+      [data-dpr="3"].tab-item4{
+        font-size:66px;
+      }
+      .tab-item5 {
+        float:left;
+
+        .px2rem(width,45);
+        .px2rem(margin-right,167);
+        font-family: PingFangSC-Regular;
+        font-size:22px;
+        color: #A5A5A5;
+        & > a {
+
+          text-decoration: none;
+          color: #A5A5A5;
+        }
+      }
+      [data-dpr="2"].tab-item5{
+        font-size:44px;
+      }
+      [data-dpr="3"].tab-item5{
+        font-size:66px;
+      }
+      .tab-item6 {
+        float:left;
+        .px2rem(width,23);
+        font-family: PingFangSC-Regular;
+        font-size:22px;
+        color: #A5A5A5;
+        & > a {
+          text-decoration: none;
+          color: #A5A5A5;
+        }
+      }
+      [data-dpr="2"].tab-item6{
+        font-size:44px;
+      }
+      [data-dpr="3"].tab-item6{
+        font-size:66px;
+      }
     }
   }
-
-
-
 
 </style>
--- src/components/heads.vue
+++ src/components/heads.vue
@@ -16,6 +16,11 @@
 </script>
 
 <style scoped lang="less">
+  @font-face{
+    font-family:PingFangSC-Semibold;
+    src:url('../assets/fonts/pfs.ttf');
+  }
+
   @baseFontSize: 75;
   .px2rem(@name, @px){
       @{name}: @px / @baseFontSize * 1rem;
@@ -24,27 +29,32 @@
     .px2rem(height,90);
     .px2rem(padding-top,35);
     .px2rem(padding-bottom,23);
-    border-bottom: 1px solid red;
+    .px2rem(width,750);
+    border-bottom: 1px solid #E2E4E6;
 
     .news-header1 {
       float: left;
-
       .px2rem(margin-left,343);
       .px2rem(margin-right,280);
-
+      .px2rem(width,65);
+      .px2rem(height,32);
       font-family: PingFangSC-Semibold;
-      font-size: 16px;
+      font-size: 32px;
       color: #333333;
-      .px2rem(line-height,33);
+      .px2rem(line-height,32);
     }
-
+    [data-dpr="2"].new-header1{
+      font-size:64px;
+    }
+    [data-dpr="3"].new-header1{
+      font-size:96px;
+    }
     .news-header2 {
       float: left;
+      .px2rem(width,32);
+      .px2rem(height,32);
       .px2rem(margin-right,30);
-      img {
-        .px2rem(height,32);
-        .px2rem(width,32);
-      }
+      
     }
   }
 
